4aa72ec8947fb5d2b31c4361cf3c3467551f683b
Falgun Malhotra <falgunmalhotra2@gmail.com>
2023-04-21 03:51:50 -0400

    Search App results updates

--- UI/ui.py
+++ UI/ui.py
@@ -20,17 +20,21 @@ def explore():
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
     #UI Parameters
-    username = str(request.form['username'])
-    userid= str(request.form['userid'])
-    userverification= str(request.form['userverification'])
-    tweetstring= str(request.form['tweetstring'])
-    hashtags= str(request.form['hashtags'])
-    tweetsensitivity = str(request.form['tweetsensitivity'])
-    tweetcontenttype= str(request.form['tweetcontenttype'])
-    datetimerange= str(request.form['datetimerange'])
-    performancestats= str(request.form['performancestats'])
-    results=fetch_results(username,userid,userverification,tweetstring,hashtags,tweetsensitivity,tweetcontenttype,datetimerange,performancestats)
-    return render_template('results.html',resultdata=results)
+    username = str(request.form['username']).strip()
+    userscreenname= str(request.form['userscreenname']).strip()
+    userverification= str(request.form['userverification']).strip()
+    tweetstring= str(request.form['tweetstring']).strip()
+    hashtags= str(request.form['hashtags']).strip()
+    tweetsensitivity = str(request.form['tweetsensitivity']).strip()
+    tweetcontenttype= str(request.form['tweetcontenttype']).strip()
+    datetimerange= str(request.form['datetimerange']).strip()
+    if('performancestats' in request.form):
+        performancestats = 1
+    else:
+        performancestats = 0
+    results_df=fetch_results(username,userscreenname,userverification,tweetstring,hashtags,tweetsensitivity,tweetcontenttype,datetimerange,performancestats)
+
+    return render_template('results.html',results=results_df.to_html(escape=False))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8000,debug=True)
--- scripts/search.py
+++ scripts/search.py
@@ -1,28 +1,53 @@
 import sys
 import os
-
+import pandas as pd
 root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(root_folder)
 
 from utils import connSQL,connNoSQL
 
-def fetch_results():
+def fetch_relational_data(SQL_client,username,userscreenname):
+    if not(username):
+        username='%'
+    if not(userscreenname):
+        userscreenname='%'
+
+    query=f'''SELECT * FROM tweets 
+                LEFT OUTER JOIN user_profile 
+                ON tweets.user_id = user_profile.user_id 
+                WHERE
+                LOWER(user_profile.name) LIKE LOWER('{username}') 
+                AND 
+                LOWER(user_profile.screen_name) LIKE LOWER('{userscreenname}')
+            '''
+    relational_data_df=pd.read_sql_query(query,con=SQL_client)
+    relational_data_df=relational_data_df.drop('tweet_created_at', axis=1)
+    all_columns_except_tweet_id=(relational_data_df.columns.difference(['tweet_id']).to_list())
+    relational_data_df= relational_data_df.groupby(all_columns_except_tweet_id,as_index=False).agg({'tweet_id':lambda x: list(x)})
+    relational_data_df=pd.concat([relational_data_df.drop(['tweet_id','tweet_flag'], axis = 1) , relational_data_df.pivot(columns = "tweet_flag", values = "tweet_id")], axis = 1).reset_index(drop = True)
+    relational_data_df['original_tweet_count'] = relational_data_df['original_tweet_flag'].apply(lambda x: f'<a value={x} href="http://example.com/">{len(x)}</a>'if isinstance(x, list) else 0)
+    relational_data_df['quoted_tweet_count'] = relational_data_df['quoted_tweet_flag'].apply(lambda x: f'<a value={x} href="http://example.com/">{len(x)}</a>'if isinstance(x, list) else 0)
+    relational_data_df['reply_tweet_count'] = relational_data_df['reply_tweet_flag'].apply(lambda x: f'<a value={x} href="http://example.com/">{len(x)}</a>'if isinstance(x, list) else 0)
+    relational_data_df['retweet_count'] = relational_data_df['retweet_flag'].apply(lambda x: f'<a value={x} href="http://example.com/">{len(x)}</a>'if isinstance(x, list) else 0)
+    columns_order=['name','screen_name','user_id', 'verified','statuses_count', 'original_tweet_count','retweet_count', 'quoted_tweet_count','reply_tweet_count','followers_count', 'friends_count', 'favourites_count','listed_count','location','language', 'description','url', 'default_profile_image','profile_image_url', 'default_profile', 'profile_background_image_url']
+    relational_data_df = relational_data_df[columns_order]
+    return(relational_data_df)
+
+
+def fetch_results(username,userscreenname,userverification,tweetstring,hashtags,tweetsensitivity,tweetcontenttype,datetimerange,performancestats):
     SQL_client=connSQL()
     NoSQL_client=connNoSQL()
-    SQL_cursor=SQL_client.cursor()
-
-    #SQL Query
-    SQL_cursor.execute('SELECT tablename FROM pg_catalog.pg_tables;')
-    for table_name in SQL_cursor:
-        print(table_name)
-    
+    relational_data_df=fetch_relational_data(SQL_client,username,userscreenname)
+    if(username or username):
+        pass
     #No SQL Query
     #es_body={'query':{"match_all": {}}}
     #resp = NoSQL_client.search(index="index_*",body=es_body)
     #print("Got %d Hits:" % resp['hits']['total']['value'])
     #for hit in resp['hits']['hits']:
         #print(hit["_source"])
-
+    results_df=relational_data_df
+    return(results_df)
 
 if __name__=='__main__':
     fetch_results()
