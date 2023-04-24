1d6ad6548fad37b9535ebf6f717e0d4afb2a687a
Falgun Malhotra <falgunmalhotra2@gmail.com>
2023-04-24 05:33:36 -0400

    Implemented Search for non-relational and Updated search for relational

--- UI/ui.py
+++ UI/ui.py
@@ -3,7 +3,6 @@ import sys
 import os
 import pandas as pd
 import numpy as np
-from datetime import datetime
 import time
 
 root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -23,27 +22,28 @@ def explore():
 
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
+    form_data = request.form.to_dict(flat=False)
     #UI Parameters
     username = str(request.form['username'])
     userscreenname= str(request.form['userscreenname']).strip()
     userverification= str(request.form['userverification']).strip()
     tweetstring= str(request.form['tweetstring']).strip()
-    hashtags= str(request.form['hashtags']).strip()
+    form_data['hashtags'] = form_data['hashtags'][1:]
+    hashtags = form_data['hashtags'][0]
     tweetsensitivity = str(request.form['tweetsensitivity']).strip()
     tweetcontenttype= str(request.form['tweetcontenttype']).strip()
     datetimerange= str(request.form['datetimerange']).strip()
     start_datetime=datetimerange.split('-')[0].strip()
     end_datetime=datetimerange.split('-')[1].strip()
-    start_datetime=datetime.strptime(start_datetime, '%m/%d/%y %I:%M %p').strftime('%Y-%m-%d %H:%M:%S')
-    end_datetime=datetime.strptime(end_datetime, '%m/%d/%y %I:%M %p').strftime('%Y-%m-%d %H:%M:%S')
-
     search_start_time=time.time()
     results_df=fetch_results(username,userscreenname,userverification,tweetstring,hashtags,tweetsensitivity,tweetcontenttype,start_datetime,end_datetime)
     search_end_time=time.time()
     total_time_taken={search_end_time-search_start_time}
-
+    if(results_df.empty):
+        results_df = pd.DataFrame(['no results found'], columns=['Message'])
     results_df.index = np.arange(1, len(results_df)+1)
     return render_template('results.html',results=results_df.to_html(escape=False),performanceresults=[total_time_taken])
+
 
 if __name__ == "__main__":
     app.run(port=8000,debug=True)
--- scripts/search.py
+++ scripts/search.py
@@ -1,84 +1,302 @@
 import sys
 import os
 import pandas as pd
+import json
+from datetime import datetime
+import numpy as np
+
 root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(root_folder)
 
 from utils import connSQL,connNoSQL
 
-def fetch_relational_data(SQL_client,username,userscreenname,userverification,start_datetime,end_datetime):
-    if not(username):
-        username='%'
-    if not(userscreenname):
-        userscreenname='%'
+# def user_summary_relational_data(SQL_client,username,userscreenname,userverification,start_datetime,end_datetime):
+#     if not(username):
+#         username='%'
+#     if not(userscreenname):
+#         userscreenname='%'
 
-    query=f'''SELECT * FROM tweets 
-                LEFT OUTER JOIN user_profile 
-                ON tweets.user_id = user_profile.user_id 
-                WHERE
-                LOWER(user_profile.name) LIKE LOWER('{username}') 
-                AND 
-                LOWER(user_profile.screen_name) LIKE LOWER('{userscreenname}')
-                AND
-                tweet_created_at BETWEEN '{start_datetime}' AND '{end_datetime}'
+#     query=f'''SELECT * FROM tweets 
+#                 LEFT OUTER JOIN user_profile 
+#                 ON tweets.user_id = user_profile.user_id 
+#                 WHERE
+#                 LOWER(user_profile.name) LIKE LOWER('{username}') 
+#                 AND 
+#                 LOWER(user_profile.screen_name) LIKE LOWER('{userscreenname}')
+#                 AND
+#                 tweet_created_at BETWEEN '{start_datetime}' AND '{end_datetime}'
+#             '''
+    
+#     #Appending query for user verified status
+#     if(userverification=='2'):
+#         query+=''' AND verified=TRUE'''
+#     if(userverification=='3'):
+#         query+=''' AND verified=FALSE'''
+    
+#     print(query)
+
+#     #run the query
+#     relational_data_df=pd.read_sql_query(query,con=SQL_client)
+
+#     #If no results found
+#     if relational_data_df.empty:
+#         return(pd.DataFrame({'message': ['sorry no values found']}))
+    
+#     #Dropping column 'tweet_created_at'
+#     relational_data_df=relational_data_df.drop('tweet_created_at', axis=1)
+    
+    
+#     #Tweets ids of same 'tweet_flag' grouped together as list
+#     all_columns_except_tweet_id=(relational_data_df.columns.difference(['tweet_id']).to_list())
+#     relational_data_df= relational_data_df.groupby(all_columns_except_tweet_id,as_index=False).agg({'tweet_id':lambda x: list(x)})
+
+#     #'tweet_flag' pivoted as columns
+#     relational_data_df=pd.concat([relational_data_df.drop(['tweet_id','tweet_flag'], axis = 1) , relational_data_df.pivot(columns = "tweet_flag", values = "tweet_id")], axis = 1).reset_index(drop = True)
+    
+#     #counting tweets
+#     if('original_tweet_flag' in relational_data_df.columns):
+#         relational_data_df['original_tweet_count'] = relational_data_df['original_tweet_flag'].apply(lambda x: f'<a value={x} href="http://example.com/">{len(x)}</a>'if isinstance(x, list) else 0)
+#     if('quoted_tweet_flag' in relational_data_df.columns):
+#         relational_data_df['quoted_tweet_count'] = relational_data_df['quoted_tweet_flag'].apply(lambda x: f'<a value={x} href="http://example.com/">{len(x)}</a>'if isinstance(x, list) else 0)
+#     if('reply_tweet_flag' in relational_data_df.columns):
+#         relational_data_df['reply_tweet_count'] = relational_data_df['reply_tweet_flag'].apply(lambda x: f'<a value={x} href="http://example.com/">{len(x)}</a>'if isinstance(x, list) else 0)
+#     if('retweet_flag' in relational_data_df.columns):
+#         relational_data_df['retweet_count'] = relational_data_df['retweet_flag'].apply(lambda x: f'<a value={x} href="http://example.com/">{len(x)}</a>'if isinstance(x, list) else 0)
+#     #ordering columns
+#     columns_order=['name','screen_name','user_id', 'verified','statuses_count', 'original_tweet_count','retweet_count', 'quoted_tweet_count','reply_tweet_count','followers_count', 'friends_count', 'favourites_count','listed_count','location','language', 'description','url', 'default_profile_image','profile_image_url', 'default_profile', 'profile_background_image_url']
+#     relational_data_df = relational_data_df[[col for col in columns_order if col in relational_data_df.columns]]
+#     return(relational_data_df)
+
+
+def fetch_searched_tweet_metadata_user_data(SQL_client,username,userscreenname,userverification,start_datetime,end_datetime,filtered_tweet_ids):
+    start_datetime=datetime.strptime(start_datetime, '%m/%d/%y %I:%M %p').strftime('%Y-%m-%d %H:%M:%S')
+    end_datetime=datetime.strptime(end_datetime, '%m/%d/%y %I:%M %p').strftime('%Y-%m-%d %H:%M:%S')
+
+    query=f'''SELECT t.*, u.name, u.screen_name,u.verified, 
+            (SELECT COUNT(r.retweet_id) FROM retweets r WHERE r.tweet_id = t.tweet_id) AS retweet_count,
+            (SELECT COUNT(q.quoted_tweet_id) FROM quoted_tweets q WHERE q.tweet_id = t.tweet_id) AS quoted_count,
+            (SELECT COUNT(p.reply_tweet_id) FROM reply p WHERE p.tweet_id = t.tweet_id) AS reply_count
+            FROM tweets t
+            JOIN user_profile u ON t.user_id = u.user_id
+            WHERE
+            t.tweet_created_at BETWEEN '{start_datetime}' AND '{end_datetime}'
             '''
     
+    if(username):
+        query+=f''' AND LOWER(u.name) LIKE LOWER('{username}') '''
+    if(userscreenname):
+        query+=f''' AND LOWER(u.screen_name) LIKE LOWER('{userscreenname}')'''
+
     #Appending query for user verified status
     if(userverification=='2'):
-        query+=''' AND verified=TRUE'''
+        query+=''' AND u.verified=TRUE'''
     if(userverification=='3'):
-        query+=''' AND verified=FALSE'''
+        query+=''' AND u.verified=FALSE'''
     
+    if(len(filtered_tweet_ids)):
+         filtered_tweet_ids=tuple(filtered_tweet_ids)
+         query+=f''' AND t.tweet_id IN {filtered_tweet_ids}'''
     print(query)
 
     #run the query
-    relational_data_df=pd.read_sql_query(query,con=SQL_client)
+    searched_tweet_metadata_user_data=pd.read_sql_query(query,con=SQL_client)
+    return(searched_tweet_metadata_user_data)
 
-    #If no results found
-    if relational_data_df.empty:
-        return(pd.DataFrame({'message': ['sorry no values found']}))
+
+def fetch_searched_tweets_data(NoSQL_client,tweetstring,hashtags,tweetsensitivity,tweetcontenttype,start_datetime,end_datetime,filtered_tweet_ids):
+    # Define search parameters
+    if(hashtags):
+        hashtags = hashtags.split(',')
+        hashtags=[x.lower() for x in hashtags]
+    start_datetime=datetime.strptime(start_datetime, '%m/%d/%y %I:%M %p').timestamp()* 1000
+    end_datetime=datetime.strptime(end_datetime, '%m/%d/%y %I:%M %p').timestamp()* 1000
+
+    # Construct Elasticsearch Query DSL
+    search_query = {
+                    "query": {
+                        "bool": {
+                        "should": [],
+                        "must":[
+                        {
+                        "range": {
+                        "created_at": {
+                            "gte": start_datetime,
+                            "lte": end_datetime
+                            }
+                        }
+                        }
+                        ],
+                        "filter": []
+                        }
+                    },
+                    "sort": [
+                        {
+                        "_score": {
+                            "order": "desc"
+                        }
+                        }
+                    ],
+                    "_source": [
+                        "id_str",
+                        "text",
+                        "entities.hashtags.text",
+                        "possibly_sensitive",
+                        "entities.media.type",
+                        "entities.media.url"
+                    ],
+                    "size":10000
+                    }
+
+    # Conditionally include the match query for text
+    if(len(tweetstring)):
+        search_query["query"]["bool"]["should"].append({
+            "match": {
+                "text": {
+                    "query": tweetstring,
+                    "operator": "or",
+                    "boost": 3
+                }
+            }
+        })
+
+    # Conditionally include the terms query for entities.hashtags.text
+    if(len(hashtags)):
+        search_query["query"]["bool"]["should"].append({
+            "terms": {
+                "entities.hashtags.text": hashtags
+            }
+        })
+
+    # Conditionally include the terms query for id_str
+    if(len(filtered_tweet_ids)):
+        search_query["query"]["bool"]["filter"].append({
+            "terms": {
+            "id_str": filtered_tweet_ids
+            }
+        })
+
+    #Condition that atleast one of tweetstring or hashtags should match if provided. 
+    if(len(tweetstring) or len(hashtags)):
+        search_query["query"]["bool"]["minimum_should_match"]=1
+
+    # Conditionally include the filter query for possibly_sensitive
+    if(tweetsensitivity) == "2":
+        search_query["query"]["bool"]["filter"].append({"term": {"possibly_sensitive": 1}})
+    if(tweetsensitivity) == "3":
+        search_query["query"]["bool"]["filter"].append({"bool": {"must_not": {"term": {"possibly_sensitive": 1}}}})
+
+    # Convert query to JSON string
+    search_query_json = json.dumps(search_query)
+    print(search_query_json)
+
+    # Execute the search query and retrieve the sorted results
+    response = NoSQL_client.search(index="index__*", body=search_query_json)
+    sorted_hits = response["hits"]["hits"]
+    searched_tweets_data = pd.DataFrame.from_records(sorted_hits)
     
-    #Dropping column 'tweet_created_at'
-    relational_data_df=relational_data_df.drop('tweet_created_at', axis=1)
+    if(searched_tweets_data.empty):
+        return(searched_tweets_data)
+    ## Extracting required columns from result dataframe
     
+    # Extracting  'possibly_sensitive'
+    searched_tweets_data['possibly_sensitive'] = searched_tweets_data['_source'].apply(lambda x:x['possibly_sensitive'])
     
-    #Tweets ids of same 'tweet_flag' grouped together as list
-    all_columns_except_tweet_id=(relational_data_df.columns.difference(['tweet_id']).to_list())
-    relational_data_df= relational_data_df.groupby(all_columns_except_tweet_id,as_index=False).agg({'tweet_id':lambda x: list(x)})
+    # Extracting 'id_str'
+    searched_tweets_data['id_str'] = searched_tweets_data['_source'].apply(lambda x:x['id_str'])
 
-    #'tweet_flag' pivoted as columns
-    relational_data_df=pd.concat([relational_data_df.drop(['tweet_id','tweet_flag'], axis = 1) , relational_data_df.pivot(columns = "tweet_flag", values = "tweet_id")], axis = 1).reset_index(drop = True)
+    # Extracting hashtags
+    def get_hashtags(row):
+        text_list = []
+        if('entities' in row.keys()):
+            if('hashtags' in row['entities']):
+                hash_list = row['entities']['hashtags']
+                for i in hash_list:
+                    text_list.append(i['text'])
+                return text_list
+            else:
+                return np.nan
+        else:
+            return np.nan
+        
+    searched_tweets_data['hashtags'] = searched_tweets_data['_source'].apply(get_hashtags)
     
-    #counting tweets
-    if('original_tweet_flag' in relational_data_df.columns):
-        relational_data_df['original_tweet_count'] = relational_data_df['original_tweet_flag'].apply(lambda x: f'<a value={x} href="http://example.com/">{len(x)}</a>'if isinstance(x, list) else 0)
-    if('quoted_tweet_flag' in relational_data_df.columns):
-        relational_data_df['quoted_tweet_count'] = relational_data_df['quoted_tweet_flag'].apply(lambda x: f'<a value={x} href="http://example.com/">{len(x)}</a>'if isinstance(x, list) else 0)
-    if('reply_tweet_flag' in relational_data_df.columns):
-        relational_data_df['reply_tweet_count'] = relational_data_df['reply_tweet_flag'].apply(lambda x: f'<a value={x} href="http://example.com/">{len(x)}</a>'if isinstance(x, list) else 0)
-    if('retweet_flag' in relational_data_df.columns):
-        relational_data_df['retweet_count'] = relational_data_df['retweet_flag'].apply(lambda x: f'<a value={x} href="http://example.com/">{len(x)}</a>'if isinstance(x, list) else 0)
+    # Extracting text
+    searched_tweets_data['text'] = searched_tweets_data['_source'].apply(lambda x:x['text'])
 
-    #ordering columns
-    columns_order=['name','screen_name','user_id', 'verified','statuses_count', 'original_tweet_count','retweet_count', 'quoted_tweet_count','reply_tweet_count','followers_count', 'friends_count', 'favourites_count','listed_count','location','language', 'description','url', 'default_profile_image','profile_image_url', 'default_profile', 'profile_background_image_url']
-    relational_data_df = relational_data_df[[col for col in columns_order if col in relational_data_df.columns]]
+    # Extracting 'media_type'
+    def get_media_type(row):
+        if 'entities' in row.keys():
+            if 'media' in row['entities']:
+                if 'type' in row['entities']['media'][0].keys():
+                    return row['entities']['media'][0]['type']
+                else:
+                    return np.nan
+            else:
+                return np.nan
+        else:
+            return np.nan
+    searched_tweets_data['media_type'] = searched_tweets_data['_source'].apply(get_media_type)
 
-    return(relational_data_df)
+    # Extracting 'media_url'
+    def get_media_url(row):
+        if 'entities' in row.keys():
+            if 'media' in row['entities']:
+                if 'url' in row['entities']['media'][0].keys():
+                    return row['entities']['media'][0]['url']
+                else:
+                    return np.nan
+            else:
+                return np.nan
+        else:
+            return np.nan
+        
+    
+    searched_tweets_data['media_url'] = searched_tweets_data['_source'].apply(get_media_url)
 
+
+    # Create a list of desired columns
+    desired_columns = ['id_str', 'text', 'hashtags','_score', 'possibly_sensitive','media_type','media_url']
+
+    #Filtering desired columns from dataframe
+    existing_desired_columns = [col for col in desired_columns if col in searched_tweets_data.columns]
+    searched_tweets_data = searched_tweets_data[existing_desired_columns]
+    
+    #Renaming tweet id column
+    searched_tweets_data = searched_tweets_data.rename(columns={'id_str': 'tweet_id'})
+    return(searched_tweets_data)
 
 def fetch_results(username,userscreenname,userverification,tweetstring,hashtags,tweetsensitivity,tweetcontenttype,start_datetime,end_datetime):
     SQL_client=connSQL()
     NoSQL_client=connNoSQL()
-    relational_data_df=fetch_relational_data(SQL_client,username,userscreenname,userverification,start_datetime,end_datetime)
-    if(username or username):
-        pass
-    #No SQL Query
-    #es_body={'query':{"match_all": {}}}
-    #resp = NoSQL_client.search(index="index_*",body=es_body)
-    #print("Got %d Hits:" % resp['hits']['total']['value'])
-    #for hit in resp['hits']['hits']:
-        #print(hit["_source"])
-    results_df=relational_data_df
+    filtered_tweet_ids=[]
+
+    #If user parameters are provided. Search relatinal->non-relational
+    if(username or userscreenname):
+        searched_tweet_metadata_user_data=fetch_searched_tweet_metadata_user_data(SQL_client,username,userscreenname,userverification,start_datetime,end_datetime,filtered_tweet_ids)
+        if(searched_tweet_metadata_user_data.empty):
+            return(searched_tweet_metadata_user_data)
+        filtered_tweet_ids = searched_tweet_metadata_user_data['tweet_id'].values.tolist()
+        filtered_tweet_ids=[str(x) for x in filtered_tweet_ids]
+        searched_tweets_data=fetch_searched_tweets_data(NoSQL_client,tweetstring,hashtags,tweetsensitivity,tweetcontenttype,start_datetime,end_datetime,filtered_tweet_ids)
+        if(searched_tweets_data.empty):
+            return(searched_tweets_data)
+
+    #If user parameters are non-provided. Search non-relatinal->relational
+    elif(tweetstring or hashtags):
+        searched_tweets_data=fetch_searched_tweets_data(NoSQL_client,tweetstring,hashtags,tweetsensitivity,tweetcontenttype,start_datetime,end_datetime,filtered_tweet_ids)
+        if(searched_tweets_data.empty):
+            return(searched_tweets_data)
+        filtered_tweet_ids = searched_tweets_data['tweet_id'].values.tolist()
+        filtered_tweet_ids=[str(x) for x in filtered_tweet_ids]
+        searched_tweet_metadata_user_data=fetch_searched_tweet_metadata_user_data(SQL_client,username,userscreenname,userverification,start_datetime,end_datetime,filtered_tweet_ids)
+        if(searched_tweet_metadata_user_data.empty):
+            return(searched_tweet_metadata_user_data)
+    
+    else:
+        return(pd.DataFrame(['Please provide one of -userid,username,tweetstring,hashtags'], columns=['Message']))
+  
+    results_df=pd.merge(searched_tweets_data,searched_tweet_metadata_user_data, on='tweet_id', how='inner') 
+    #results_df=searched_tweet_metadata_user_data
     return(results_df)
 
 if __name__=='__main__':
