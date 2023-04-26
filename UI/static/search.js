82aa17fc1ffe948ef7ef4b022d053785bcc3608b
Falgun Malhotra <falgunmalhotra2@gmail.com>
2023-04-26 17:20:01 -0400

    Integrating SearchApp

---
+++ UI/static/search.js
@@ -0,0 +1,8 @@
+$("#Input4").daterangepicker({
+    timePicker: true,
+    startDate: '04/12/20 00:00 AM',
+    endDate: '04/26/20 11:59 PM',
+    locale: {
+        format: "MM/DD/YY hh:mm A"
+    }
+});
