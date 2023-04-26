53616fcae1363d52cfd6a85d082273c3071fdf82
advaithsrao <advaith652@gmail.com>
2023-04-25 22:41:50 -0400

    Added V1 cache into codebase

---
+++ scripts/cache.py
@@ -0,0 +1,136 @@
+"""
+File : /scripts/cache.py
+
+Description:
+1. Implement the cache function for our twitter search applet
+2. Optimally store/retrieve cache info from/to the disk
+"""
+import json
+import os
+from collections import OrderedDict
+import asyncio
+from datetime import datetime
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return super().default(obj)
+
+class CacheManager:
+    def __init__(self, cache_file_path = "./data/lruCache.json", max_size=1024):
+        """
+        Initializes a new instance of the DiskLRUCache class.
+
+        Parameters:
+        cache_file_path (str): The path to the file that will be used for caching.
+        max_size (int): The maximum number of entries that can be stored in the cache.
+        """
+        self.cache_file_path = cache_file_path
+        
+        self.max_size = max_size
+        
+        # Load cache file
+        self.cache = self.loadCache()
+
+    def loadCache(self):
+        """
+        Loads the cache from the file on disk.
+        """
+        try:
+            if os.path.exists(self.cache_file_path):
+                with open(self.cache_file_path, 'r') as f:
+                    return OrderedDict(json.load(f))
+            else:
+                return OrderedDict()
+        except Exception as e:
+            print(f'Cache Load Failed as: {e}. Defaulting to empty cache')
+            return OrderedDict()
+
+    async def saveCache(self):
+        """
+        Saves the cache to the file on disk.
+        """
+        with open(self.cache_file_path, 'w') as f:
+            json.dump(self.cache, f)
+    
+    def __contains__(self, key):
+        """
+        Returns True if the key is in the cache, False otherwise.
+
+        Parameters:
+        key (str): The key to search for in the cache.
+        """
+        if isinstance(key, str):
+            return key in self.cache
+        elif isinstance(key, dict):
+            return json.dumps(key) in self.cache
+
+    def getQuery(self, key):
+        """
+        Returns the value associated with the given key in the cache, else returns None
+
+        Parameters:
+        key (str): The key to retrieve the value for.
+        """
+        if isinstance(key, dict):
+            key = json.dumps(key)
+
+        # Add the key as new entry to the dict by moving it to top of the order
+        value = self.cache.pop(key)
+        
+        self.cache[key] = value
+
+        return value['result']
+
+    def setQuery(self, key, result, response_time):
+        """
+        Adds the given key-value pair to the cache.
+
+        Parameters:
+        key (str): The key to add to the cache.
+        results (any): The value to add to the cache.
+        """
+        if response_time < 10:
+            _ttl = 300
+        else:
+            _ttl = 300 + response_time
+
+        if isinstance(key, dict):
+            key = json.dumps(key)
+        
+        if len(self.cache) >= self.max_size:
+            self.cache.popitem(last=False)
+        
+        self.cache[key] = {
+            'result'        : result, 
+            'time-to-live'  : _ttl,
+            'created_at'    : datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            'response_time' : response_time
+        }
+
+    def delQuery(self, key):
+        """
+        Removes the given key-value pair from the cache.
+
+        Parameters:
+        key (str): The key to remove from the cache.
+        """
+        if isinstance(key, dict):
+            key = json.dumps(key)
+        
+        del self.cache[key]
+        
+
+    def clear(self):
+        """
+        Clears all items from the cache.
+        """
+        self.cache.clear()
+    
+    def close(self, save = True):
+        """
+        Saves the cache to the JSON file and close it
+        """
+        if save:
+            asyncio.run(self.saveCache())
